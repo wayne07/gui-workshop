94a04316f1d57a8c2315f3ec6c3591024d536817
Jürgen Seliger <juergen.seliger@idos.de>
2014-05-28 13:33:25 +0200

    gui erweitert

--- chronos-command/src/main/java/de/idos/chronos/ChronosGui.java
+++ chronos-command/src/main/java/de/idos/chronos/ChronosGui.java
@@ -1,5 +1,8 @@
 package de.idos.chronos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -8,38 +11,70 @@ import org.joda.time.LocalTime;
 
 class ChronosGui {
 
-    private final JLabel separator1 = new JLabel(":");
-    private final JLabel separator2 = new JLabel(":");
-    private final JLabel hour = new JLabel();
-    private final JLabel minute = new JLabel();
-    private final JLabel second = new JLabel();
-
     private final JPanel panel = new JPanel();
 
-    private final GuiUpdater update = new GuiUpdater(hour, minute, second);
+    private final List<TimePanel> panels = new ArrayList<TimePanel>();
 
     public ChronosGui() {
-        createContent();
+        panels.add(new TimePanel(""));
+        panels.add(new TimePanel("Frankfurt"));
+        panels.add(new TimePanel("Tokio"));
+        panels.add(new TimePanel("New York"));
+
+        for (TimePanel singlePanel : panels) {
+            panel.add(singlePanel.getComponent());
+        }
     }
 
-    private JPanel createContent() {
-        JLabel label = new JLabel("Uhrzeit: ");
+    static class TimePanel {
 
-        panel.add(label);
-        panel.add(hour);
-        panel.add(separator1);
-        panel.add(minute);
-        panel.add(separator2);
-        panel.add(second);
-        return panel;
+        private final JLabel separator1 = new JLabel(":");
+        private final JLabel separator2 = new JLabel(":");
+        private final JLabel hour = new JLabel();
+        private final JLabel minute = new JLabel();
+        private final JLabel second = new JLabel();
+
+        private final JPanel panel = new JPanel();
+        private final GuiUpdater update = new GuiUpdater(hour, minute, second);
+
+        public TimePanel(String name) {
+            createPanelWithTime(name);
+        }
+
+        public JPanel createPanelWithTime(String name) {
+            JLabel label = new JLabel("Uhrzeit in " + name + ": ");
+            if (name.isEmpty()) {
+                label.setText("Uhrzeit: ");
+            }
+
+            panel.add(label);
+            panel.add(hour);
+            panel.add(separator1);
+            panel.add(minute);
+            panel.add(separator2);
+            panel.add(second);
+            return panel;
+        }
+
+        public JComponent getComponent() {
+            return panel;
+        }
+
+        public void setTime(LocalTime time) {
+            update.updateTime(time);
+        }
+
     }
+
 
     public JComponent getComponent() {
         return panel;
     }
 
     public void setTime(LocalTime time) {
-        update.updateTime(time);
+        for (TimePanel panel : panels) {
+            panel.setTime(time);
+        }
     }
 
 }

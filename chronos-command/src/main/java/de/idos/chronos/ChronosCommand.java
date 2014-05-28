6f7e93219bb1787a7eb755b9c7d00c09b1bf8ee4
Jürgen Seliger <juergen.seliger@idos.de>
2014-05-28 10:41:49 +0200

    aasdf

--- chronos-command/src/main/java/de/idos/chronos/ChronosCommand.java
+++ chronos-command/src/main/java/de/idos/chronos/ChronosCommand.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 public class ChronosCommand {
 
@@ -12,16 +14,24 @@ public class ChronosCommand {
         if (args.length < 1) {
             System.out.println("Ich brauche die millis seit 1970, z.B. " + new Date().getTime());
         }
+
+        DateTime dateTime = new DateTime(Long.parseLong(args[0]), DateTimeZone.UTC);
+
+        DateTime athensTime = dateTime.withZone(DateTimeZone.forID("Europe/Athens"));
+
+        System.out.println(athensTime.toString());
+
         Long mylong = new Long(args[0]);
         Date date = new Date(mylong);
+
         int dateStyle = SimpleDateFormat.LONG;
         int timeStyle = SimpleDateFormat.LONG;
-        //        Locale aLocale = new Builder().setLanguage("ell").build();
-        Locale aLocale = new Locale("el-GR");
+        Locale aLocale = new Locale("GR");
 
         DateFormat dateFormat = SimpleDateFormat.getDateTimeInstance(dateStyle, timeStyle, aLocale);
 
 
         System.out.println("Da hasch Dei Zeit: " + dateFormat.format(date));
     }
+
 }

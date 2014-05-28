f12606adb325c2ae7738e4f1d34d7d3a328010fc
Jürgen Seliger <juergen.seliger@idos.de>
2014-05-28 13:50:21 +0200

    TimePanel in separater Klasse

--- chronos-command/src/main/java/de/idos/chronos/ChronosGui.java
+++ chronos-command/src/main/java/de/idos/chronos/ChronosGui.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.joda.time.DateTime;
@@ -26,48 +25,6 @@ class ChronosGui {
             panel.add(singlePanel.getComponent());
         }
     }
-
-    static class TimePanel {
-
-        private final JLabel separator1 = new JLabel(":");
-        private final JLabel separator2 = new JLabel(":");
-        private final JLabel hour = new JLabel();
-        private final JLabel minute = new JLabel();
-        private final JLabel second = new JLabel();
-
-        private final JPanel panel = new JPanel();
-        private final GuiUpdater updater;
-
-        public TimePanel(String name, DateTimeZone timeZone) {
-            createPanelWithTime(name);
-            updater = new GuiUpdater(hour, minute, second, timeZone);
-        }
-
-        public JPanel createPanelWithTime(String name) {
-            JLabel label = new JLabel("Uhrzeit in " + name + ": ");
-            if (name.isEmpty()) {
-                label.setText("Uhrzeit: ");
-            }
-
-            panel.add(label);
-            panel.add(hour);
-            panel.add(separator1);
-            panel.add(minute);
-            panel.add(separator2);
-            panel.add(second);
-            return panel;
-        }
-
-        public JComponent getComponent() {
-            return panel;
-        }
-
-        public void setTime(DateTime time) {
-            updater.updateTime(time);
-        }
-
-    }
-
 
     public JComponent getComponent() {
         return panel;
---
+++ chronos-command/src/main/java/de/idos/chronos/TimePanel.java
@@ -0,0 +1,49 @@
+package de.idos.chronos;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+class TimePanel {
+
+    private final JLabel separator1 = new JLabel(":");
+    private final JLabel separator2 = new JLabel(":");
+    private final JLabel hour = new JLabel();
+    private final JLabel minute = new JLabel();
+    private final JLabel second = new JLabel();
+
+    private final JPanel panel = new JPanel();
+    private final GuiUpdater updater;
+
+    public TimePanel(String name, DateTimeZone timeZone) {
+        createPanelWithTime(name);
+        updater = new GuiUpdater(hour, minute, second, timeZone);
+    }
+
+    public JPanel createPanelWithTime(String name) {
+        JLabel label = new JLabel("Uhrzeit in " + name + ": ");
+        if (name.isEmpty()) {
+            label.setText("Uhrzeit: ");
+        }
+
+        panel.add(label);
+        panel.add(hour);
+        panel.add(separator1);
+        panel.add(minute);
+        panel.add(separator2);
+        panel.add(second);
+        return panel;
+    }
+
+    public JComponent getComponent() {
+        return panel;
+    }
+
+    public void setTime(DateTime time) {
+        updater.updateTime(time);
+    }
+
+}

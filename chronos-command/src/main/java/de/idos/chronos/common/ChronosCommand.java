f05a5f6678ef044201731de0c99729236b7f25ca
Jürgen Seliger <juergen.seliger@idos.de>
2014-05-28 14:21:24 +0200

    in package organisiert

---
+++ chronos-command/src/main/java/de/idos/chronos/common/ChronosCommand.java
@@ -0,0 +1,32 @@
+package de.idos.chronos.common;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+public class ChronosCommand {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Ich brauche die millis seit 1970, z.B. " + new Date().getTime());
+        }
+
+        DateTime dateTime = new DateTime(Long.parseLong(args[0]), DateTimeZone.UTC);
+
+        DateTime athensTime = dateTime.withZone(DateTimeZone.forID("Europe/Athens"));
+
+        System.out.println(athensTime.toString());
+
+        Long mylong = new Long(args[0]);
+        Date date = new Date(mylong);
+
+        SimpleDateFormat isoFormat = new SimpleDateFormat("HH:mm:ss");
+        isoFormat.setTimeZone(TimeZone.getTimeZone("Europe/Athens"));
+
+        System.out.println("Da hasch Dei Zeit: " + isoFormat.format(date));
+    }
+
+}
